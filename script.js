8852e6a4110e72910390b87c1c29eef113aabfda
AbdelRahman Mohammad <3bdlr7mn@gmail.com>
2022-11-14 12:48:31 +0300

    echo bot ready

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ require([
   // programmatic
   "dojo/dom",
   "dojo/on",
+  "dojo/dom-construct",
   // declarative
   "dojo/parser",
   "dijit/form/TextBox",
   "dojo/domReady!",
-], function (dom, on) {
+], function (dom, on, domConstruct) {
+  var messagesContainer = dom.byId("messages");
   var inputField = dom.byId("input");
 
   // when inside the input field an enter key is pressed
@@ -16,9 +18,41 @@ require([
     if (e.keyCode === 13) {
       let input = inputField.value;
       inputField.value = "";
-      output(input);
+      addChatEntry(input);
     }
   });
-});
 
-function output(input) {}
+  function addChatEntry(input) {
+    // creating a div with the input text inside it as a user response
+    // then palcing that text at the last child of the messages container
+    var userDiv = domConstruct.toDom(
+      `<div id="user" class="user response">${input}</div>`
+    );
+    domConstruct.place(userDiv, messagesContainer, "last");
+
+    // creating a div with the input text inside it as a bot response
+    // then palcing that text at the last child of the messages container
+    var botDiv = domConstruct.toDom(
+      `<div id="bot" class="bot response">${input}</div>`
+    );
+    domConstruct.place(botDiv, messagesContainer, "last");
+
+    // let userDiv = document.createElement("div");
+    // userDiv.id = "user";
+    // userDiv.className = "user response";
+    // userDiv.innerHTML = `${input}`;
+    // messagesContainer.appendChild(userDiv);
+
+    // let botDiv = document.createElement("div");
+    // let botText = document.createElement("span");
+    // botDiv.id = "bot";
+    // botDiv.className = "bot response";
+    // botText.innerText = "Typing...";
+    // botDiv.appendChild(botText);
+    // messagesContainer.appendChild(botDiv);
+
+    // setTimeout(() => {
+    //   botText.innerText = `${product}`;
+    // }, 2000);
+  }
+});
