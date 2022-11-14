79066bf7b24a1bc5eec4a50a4e0d56a3034330d7
AbdelRahman Mohammad <3bdlr7mn@gmail.com>
2022-11-14 12:21:48 +0300

    input field working

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ require([
   "dojo/parser",
   "dijit/form/TextBox",
   "dojo/domReady!",
-], function (dom, on) {});
+], function (dom, on) {
+  var inputField = dom.byId("input");
+
+  // when inside the input field an enter key is pressed
+  // save the input field value in a variable and then reset it
+  // and a function is run with the input value passed as param
+  on(inputField, "keydown", function (e) {
+    if (e.keyCode === 13) {
+      let input = inputField.value;
+      inputField.value = "";
+      output(input);
+    }
+  });
+});
+
+function output(input) {}
